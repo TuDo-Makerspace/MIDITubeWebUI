077d0ee4adaedc4c93d0f148c0f60827e20abac7
koljapluemer <github@koljapluemer.com>
2025-03-20 22:03:26 +0100

    minor layout improve

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@ import Room from './components/Room.vue';
 <template>
   <div class="min-h-screen bg-base-200 p-4">
     <div class="container mx-auto">
-      <h1 class="text-3xl font-bold text-center mb-8">Smart Lamp Controller</h1>
       <Room />
     </div>
   </div>
